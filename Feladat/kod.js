676e0b8b6e1543a13458d45126134822e10d48fd
kytrack <70961365+kytrack@users.noreply.github.com>
2022-10-28 10:16:09 +0200

    Add files via upload

---
+++ Feladat/kod.js
@@ -0,0 +1,14 @@
+function adatBanyasz(){
+    var nev=document.getElementById("megrendelonevid").value;
+	document.getElementById("nev").innerHTML="Név: "+nev;
+
+    var cim=document.getElementById("megrendelocimid").value;
+	document.getElementById("cim").innerHTML="Cím:"+nev;
+
+    var telo=document.getElementById("telefon").value;
+	document.getElementById("telo").innerHTML="Telefon: "+nev;
+}
+
+function fokuszban(){
+    document.getElementsByTagName(input)
+}
