55cd889544561175bfdd485fb1c40b2529fc4a56
kytrack <70961365+kytrack@users.noreply.github.com>
2022-11-02 20:28:08 +0100

    Add files via upload

--- Feladat/kod.js
+++ Feladat/kod.js
@@ -3,12 +3,29 @@ function adatBanyasz(){
 	document.getElementById("nev").innerHTML="Név: "+nev;
 
     var cim=document.getElementById("megrendelocimid").value;
-	document.getElementById("cim").innerHTML="Cím:"+nev;
+	document.getElementById("cim").innerHTML="Cím:"+cim;
 
-    var telo=document.getElementById("telefon").value;
-	document.getElementById("telo").innerHTML="Telefon: "+nev;
+    var telo=document.getElementById("telefonid").value;
+	document.getElementById("telo").innerHTML="Telefon: "+telo;
 }
 
-function fokuszban(){
-    document.getElementsByTagName(input)
+function fokuszban1(){
+    document.getElementById("megrendelonevid").style.backgroundColor = "rgba(0, 0, 2000, 0.10)";
+}
+function fokuszban2(){
+    document.getElementById("megrendelocimid").style.backgroundColor = "rgba(0, 0, 2000, 0.10)";
+}
+function fokuszban3(){
+    document.getElementById("telefonid").style.backgroundColor = "rgba(0, 0, 2000, 0.10)";
+}
+
+
+function nincsfokuszban1(){
+    document.getElementById("megrendelonevid").style.backgroundColor = "white";
+}
+function nincsfokuszban2(){
+    document.getElementById("megrendelocimid").style.backgroundColor = "white";
+}
+function nincsfokuszban3(){
+    document.getElementById("telefonid").style.backgroundColor = "white";
 }
